96214bda87c8ef5b501e593a94ca22476298d439
Pwhitlock <pwhitlock131@gmail.com>
2023-10-07 16:46:09 -0500

    Lessened the use of the word 'question' in the JavaScript and added 'prompt' Also updated README file

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,4 +1,5 @@
 var timeSpan = document.querySelector("#time-left");
+
 var startBttn = document.getElementById("start-bttn");
 var questionContainer = document.getElementById("question-container")
 var highScores = [];
@@ -7,59 +8,68 @@ var scoreInputForm = document.getElementById("score-form");
 var saveScoreButton = document.getElementById("save-score");
 var selectedAnswers = [];
 
-// questions segment
-var questions = [
+// questionBank segment
+var questionBank = [
     {
-        question: "Question 1. Inside which HTML element do we put the JavaScript?",
-        choices: [" A. <js>", "B. <script>", "C. <java>"],
-        answer: "B.",
+        prompt: "Question 1. Inside which HTML element do we put the JavaScript?",
+        choices: ["A. <js>", "B. <script>", "C. <java>"],
+        answer: "B. <script>",
     },
     {
-        question: "Question 2. What is the correct syntax for referring to an external script called blank.js?",
+        prompt: "Question 2. What is the correct syntax for referring to an external script called blank.js?",
         choices: [" A. <script href=blank.js>", "B. <script=blank.js>", "C. <script src=blank.js>"],
         answer: "C.",
     },
     {
-        question: "Question 3. How do you write (Hello World) in an alert box?",
+        prompt: "Question 3. How do you write (Hello World) in an alert box?",
         choices: [" A. alert('Hello World');", "B. msg('Hello World');", "C. box('Hello World');"],
         answer: "A.",
     },
     {
-        question: "Question 4. How do you create a function in JavaScript?",
+        prompt: "Question 4. How do you create a function in JavaScript?",
         choices: [" A. function = myFunction()", "B. function myFunction()", "C. function: myFunction()"],
         answer: "B.",
     },
     {
-        question: "Question 5. How do you call a function named 'myFunction'",
+        prompt: "Question 5. How do you call a function named 'myFunction'",
         choices: [" A. myFunction()", "B. call.myFunction()", "C. myFunction"],
         answer: "A.",
     },
 ];
+console.log(questionBank[0].choices)
 
 
-var timeLeft = questions.length * 10;
+var timeLeft = questionBank.length * 10;
 var currentQuestionIndex = 0;
-var selectedAnswers = new Array(questions.length);
+var selectedAnswers = new Array(questionBank.length);
 
 function addChoiceEventListeners() {
+    console.log("addingChoiceEventListeners");
     var choiceInputs = document.querySelectorAll('input[name="choice"]');
+    console.log(choiceInputs)
     for (var i = 0; i < choiceInputs.length; i++) {
-        choiceInputs[i].addEventListener("change", function () {
-            if (this.checked) {
-                selectedAnswers[currentQuestionIndex] = this.value;
+        choiceInputs[i].addEventListener("change", function (event) {
+            event.preventDefault()
+            console.log(event.target)
+            console.dir(event.target)
+            console.log(event.target.parentElement)
+            var answerSelected = event.target.parentElement.innerText
+            if(answerSelected !== questionBank[currentQuestionIndex].answer){
+                timeLeft -=5
+            }
                 currentQuestionIndex++;
                 displayQuestion();
-            }
+            
         });
     }
 }
 
 function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        var currentQuestion = questions[currentQuestionIndex];
+    if (currentQuestionIndex < questionBank.length) {
+        var currentQuestion = questionBank[currentQuestionIndex];
         var questionElement = document.createElement("div");
         questionElement.className = "question";
-        questionElement.textContent = currentQuestion.question;
+        questionElement.textContent = currentQuestion.prompt;
 
         for (var i = 0; i < currentQuestion.choices.length; i++) {
             var choiceLabel = document.createElement("label");
@@ -139,7 +149,7 @@ function displayHighScores() {
 
 function isQuizComplete() {
 
-    return currentQuestionIndex >= questions.length || timeLeft <= 0;
+    return currentQuestionIndex >= questionBank.length || timeLeft <= 0;
 }
 
 function calculateScore() {
@@ -147,12 +157,12 @@ function calculateScore() {
 
     var correctAnswers = 0;
 
-    for (var i = 0; i < questions.length; i++) {
-        if (questions[i].answer === selectedAnswers[i]) {
+    for (var i = 0; i < questionBank.length; i++) {
+        if (questionBank[i].answer === selectedAnswers[i]) {
             correctAnswers++;
         }
     }
-    var totalQuestions = questions.length;
+    var totalQuestions = questionBank.length;
     var userScore = (correctAnswers / totalQuestions) * 100;
     return userScore.toFixed(2);
 }
